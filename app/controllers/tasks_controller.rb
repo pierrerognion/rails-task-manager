f798c5b2d2f6aed1731bebd0481b45a3df23b77e
Pierre Rognion <pierrerognion@gmail.com>
2019-08-13 12:37:24 +0200

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    # @tasks = Task.find(params[:id])
+    # @task.update(tasks_params)
+    # redirect_to tasks_params(@tasks)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 end
